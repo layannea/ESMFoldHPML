cd8b1b90bd69dc2f39966db4604469430754087b
Stone Su <sun.stones2003@gmail.com>
2025-12-16 02:22:07 +0000

    added wandb as well as casp14 dataset

--- profiler.py
+++ profiler.py
@@ -9,11 +9,8 @@ class Profiler:
     def __init__(self, enabled: bool = False):
         self.enabled = enabled
         self.use_cuda = torch.cuda.is_available()
-        self.timings: Dict[str, List[float]] = {}
 
-    def __str__(self):
-
-        relevant_events = {
+        self.relevant_events = {
             "INPUT_EMBEDDING" : [
                 "esm2_encoding",
                 "esm_preprocessing",
@@ -46,13 +43,16 @@ class Profiler:
                 "prediction_heads",
             ],
         }
+        self.timings: Dict[str, List[float]] = {}
+
+    def __str__(self):
 
         max_key_len = max(len(k) for k in self.timings)
 
         forwardpass_total = float(np.sum(self.timings["total_forward"]))
         
         res = ""
-        for group_name, events in relevant_events.items():
+        for group_name, events in self.relevant_events.items():
             res += f"{group_name}\n"
             res += "=" * 15 + "\n"
         
@@ -67,9 +67,24 @@ class Profiler:
             res += f"{'total':<{max_key_len}}  {group_total:>10.4f}   ({percent:.2f} %)\n\n"
         
         return res
+    
+    def percents(self):
+        forwardpass_total = float(np.sum(self.timings["total_forward"]))
+        res = []
+        for group_name, events in self.relevant_events.items():
+
+            group_total = 0.0
+            for event in events:
+                total = float(np.sum(self.timings[event]))
+                group_total += total
+
+            percent = 100.0 * group_total / forwardpass_total
+
+            res.append((group_name, percent))
+        return res
 
     def reset(self):
-        self.timings.clear
+        self.timings.clear()
 
     @contextmanager
     def profile(self, name: str):
